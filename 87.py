29081395f802485df7391c0225df138f8f40b7f2
bajjurisupraja <42571435+bajjurisupraja@users.noreply.github.com>
2018-12-10 11:29:39 +0530

    Update 87.py

--- 87.py
+++ 87.py
@@ -1,8 +1,8 @@
-c,d=map(int,raw_input().split())
-def gcd(p,q):
-    s=abs(p-q)
-    if (p-q)==0:
-        return q
+a,b=map(int,raw_input().split())
+def gcd(p,r):
+    s=abs(p-r)
+    if (p-r)==0:
+        return r
     else:
-        return gcd(s,min(p,q))
-print gcd(c,d)
+        return gcd(s,min(p,r))
+print gcd(a,b)
