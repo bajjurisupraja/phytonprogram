52e935f7cd662b0cc5898ee133b661dc7efa535a
bajjurisupraja <42571435+bajjurisupraja@users.noreply.github.com>
2018-12-14 12:38:00 +0530

    Update 76.py

--- 76.py
+++ 76.py
@@ -1,10 +1,10 @@
 m=int(raw_input())
 count=0
 if m>1:
-    for i in range(2,m):
+    for i in range(3,m):
         if m%i==0:
             count=count+1
 if count>1:
-    print "yes"
-else:
     print "no"
+else:
+    print "yes"
