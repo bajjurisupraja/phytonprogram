e3c4c31601317de5dccb2f0308fa13e846fbdb5f
bajjurisupraja <42571435+bajjurisupraja@users.noreply.github.com>
2018-12-14 12:02:45 +0530

    Update 98.py

--- 98.py
+++ 98.py
@@ -1,13 +1,13 @@
 #include<stdio.h>
 void main()
 {
-    int a[10],i,max,n;
+    int a[20],i,max,n;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
     scanf("%d",&a[i]);
     }
-    max=a[0];
+    max=a[1];
     for(i=0;i<n;i++)
     {
         if(max>a[i])
